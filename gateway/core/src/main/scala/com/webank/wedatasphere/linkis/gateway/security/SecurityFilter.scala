3c924b77125e9e5d728ab19677928e9249bb533d
jftang <patinousward@aliyun.com>
2019-12-18 18:10:24 +0800

    add comment

--- gateway/core/src/main/scala/com/webank/wedatasphere/linkis/gateway/security/SecurityFilter.scala
+++ gateway/core/src/main/scala/com/webank/wedatasphere/linkis/gateway/security/SecurityFilter.scala
@@ -49,8 +49,10 @@ object SecurityFilter {
     gatewayContext.getResponse.write(message)
     gatewayContext.getResponse.sendResponse()
   }
-
+  //这个Filter 是springcloud转发的时候进行调用的,dofilter返回true说明ok,返回false说明不允许访问
+  //请求都是在这里做的ticket校验
   def doFilter(gatewayContext: GatewayContext): Boolean = {
+    //给response加一些跨域访问的内容
     addAccessHeaders(gatewayContext)
     if(refererValidate) {
       //Security certification support, referer limited(安全认证支持，referer限定)
@@ -67,7 +69,10 @@ object SecurityFilter {
           return false
       }
     }
+    //isPassAuthRequest免校验的requesturl的记录
     val isPassAuthRequest = GatewayConfiguration.PASS_AUTH_REQUEST_URI.exists(gatewayContext.getRequest.getRequestURI.startsWith)
+    //如果是/user/login  logout  等..url的,就进行登陆操作
+    //已经登陆会抛出异常
     if(gatewayContext.getRequest.getRequestURI.startsWith(ServerConfiguration.BDP_SERVER_USER_URI.getValue)) {
       Utils.tryCatch(userRestful.doUserRequest(gatewayContext)){ t =>
         val message = t match {
@@ -78,18 +83,27 @@ object SecurityFilter {
         filterResponse(gatewayContext, Message.error(message).<<(gatewayContext.getRequest.getRequestURI))
       }
       false
-    } else if(isPassAuthRequest && !GatewayConfiguration.ENABLE_SSO_LOGIN.getValue) {
+    }
+      //如果是免登陆的url地址而且不允许sso登陆,直接不校验返回true
+    else if(isPassAuthRequest && !GatewayConfiguration.ENABLE_SSO_LOGIN.getValue) {
       GatewaySSOUtils.info("No login needed for proxy uri: " + gatewayContext.getRequest.getRequestURI)
       true
-    } else if(TokenAuthentication.isTokenRequest(gatewayContext)) {
+    }
+    //如果是token校验,则直接校验token.properties中的token用户,有不报错,没有抛出异常
+    else if(TokenAuthentication.isTokenRequest(gatewayContext)) {
       TokenAuthentication.tokenAuth(gatewayContext)
     } else {
+      //剩余的就普通请求,getLoginUser  看下用户有没登陆
       val userName = Utils.tryCatch(GatewaySSOUtils.getLoginUser(gatewayContext)){
+        //如果抛出的异常是NonLoginException  或者登陆过期
         case n @ (_: NonLoginException | _: LoginExpireException )=>
+          //如果没登陆,但是开启了testmode,就返回None了
           if(Configuration.IS_TEST_MODE.getValue) None else {
+            //如果未登陆,也没开启testmode,就返回未登陆的报错
             filterResponse(gatewayContext, Message.noLogin(n.getMessage) << gatewayContext.getRequest.getRequestURI)
             return false
           }
+          //其他异常
         case t: Throwable =>
           GatewaySSOUtils.warn("", t)
           throw t
@@ -97,24 +111,30 @@ object SecurityFilter {
       if(userName.isDefined) {
         true
       } else if(Configuration.IS_TEST_MODE.getValue) {
+        //开启testMode就返回wds.linkis.test.user的变量名
         GatewaySSOUtils.info("test mode! login for uri: " + gatewayContext.getRequest.getRequestURI)
         GatewaySSOUtils.setLoginUser(gatewayContext, testUser)
         true
       } else if(GatewayConfiguration.ENABLE_SSO_LOGIN.getValue) {
+        //如果允许sso登陆(这里的sso指自定义的sso,而非ldap,但是SSOutils 这个概念倒是包含了ldap登陆)
         val user = SSOInterceptor.getSSOInterceptor.getUser(gatewayContext)
         if(StringUtils.isNotBlank(user)) {
           GatewaySSOUtils.setLoginUser(gatewayContext.getRequest, user)
           true
-        } else if(isPassAuthRequest) {
+        }
+        //sso也没有或则返回null的话,看下这个url是否是免验证的,免登陆的就重定向
+        else if(isPassAuthRequest) {
           gatewayContext.getResponse.redirectTo(SSOInterceptor.getSSOInterceptor.redirectTo(gatewayContext.getRequest.getURI))
           gatewayContext.getResponse.sendResponse()
           false
         } else {
+          //其他一律返回false
           filterResponse(gatewayContext, Message.noLogin("You are not logged in, please login first(您尚未登录，请先登录)!")
             .data("enableSSO", true).data("SSOURL", SSOInterceptor.getSSOInterceptor.redirectTo(gatewayContext.getRequest.getURI)) << gatewayContext.getRequest.getRequestURI)
           false
         }
       } else {
+        //其他一律返回false
         filterResponse(gatewayContext, Message.noLogin("You are not logged in, please login first(您尚未登录，请先登录)!") << gatewayContext.getRequest.getRequestURI)
         false
       }
